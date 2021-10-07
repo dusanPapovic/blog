318b3216081cfcb376435f6eaf4c253ed1b451f0
dusanPapovic <papovicdusan91@gmail.com>
2021-10-07 20:16:36 +0200

    first task

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+
+        $posts=Post::all();
+//dd($posts);
+info($posts);
+return view('posts', compact('posts'));
+    }
+
+    public function show($id)
+{
+    $post=Post::findOrFail($id);
+    
+    return view('post', compact('post'));
+}
+}
+
