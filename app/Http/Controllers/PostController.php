e47c2d923fe090076e5d765f867d286a49b65e30
dusanPapovic <papovicdusan91@gmail.com>
2021-10-07 21:19:47 +0200

    4nd task

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,23 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class PostController extends Controller
 {
     public function index()
     {
 
-        $posts=Post::all();
-//dd($posts);
-info($posts);
-return view('posts', compact('posts'));
+        $posts = Post::unpublished()->get();
+        //dd($posts);
+        info($posts);
+        return view('posts', compact('posts'));
     }
 
     public function show(Post $post)
-{
-    //$post=Post::findOrFail($id);
+    {
+        //$post=Post::findOrFail($id);
+        if ($post->is_published) {
+            throw new ModelNotFoundException;
+        }
 
-    return view('post', compact('post'));
+        return view('post', compact('post'));
+    }
 }
-}
-
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -22,13 +22,13 @@
 @section('title','Blog')
 @section('content')
 <h1>Posts</h1>
-        <ul>
-            @foreach($posts as $post)
-                <li>
-                    <a href="{{route('post',['post'=> $post->id])}}">
-                        {{$post->title}}
-                    </a>
-                </li>
-            @endforeach
-        </ul>
+<ul>
+    @foreach($posts as $post)
+    <li>
+        <a href="{{route('post',['post'=> $post->id])}}">
+            {{$post->title}}
+        </a>
+    </li>
+    @endforeach
+</ul>
 @endsection
